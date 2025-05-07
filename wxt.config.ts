a234aa12e3de6386919360f2e57f0ae54c0f9aa4
zhangning <7148505@qq.com>
2025-05-07 15:24:44 +0800

    🔧 chore(deps): 添加echarts依赖并配置扩展图标
    - 添加echarts和echarts-for-react依赖包
    - 更新pnpm-lock.yaml文件
    -【配置】在manifest中添加扩展图标和标题配置

--- wxt.config.ts
+++ wxt.config.ts
@@ -13,7 +13,17 @@ export default defineConfig({
     ],
     host_permissions: [
       '<all_urls>'
-    ]
+    ],
+    "action": {
+      "default_icon": {
+        "16": "icon/16.png",
+        "32": "icon/32.png",
+        "48": "icon/48.png",
+        "96": "icon/96.png",
+        "128": "icon/128.png"
+      },
+      "default_title": "浏览时间统计"
+    },
   },
   modules: ['@wxt-dev/module-react'],
 })
